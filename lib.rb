0347998d24e0aff996fd597290a45823d0287ad1
Carter Tsai <hamming1@gmail.com>
2014-04-18 15:32:37 +0800

    Add send header feature

--- lib.rb
+++ lib.rb
@@ -1,9 +1,22 @@
 require 'net/http'
 require 'json'
+require "addressable/uri"
 
 module MHTTP
-    def MHTTP.post(url, data)
-        res = Net::HTTP.post_form(url, data)
+    def MHTTP.post(uri, data, header=nil)
+        req = Net::HTTP.new(uri.host, uri.port)
+        params = Addressable::URI.new
+        params.query_values = data
+
+        if header != nil
+            cookie = header.split('; ')[0]
+            puts cookie
+            headers = {
+                'Cookie' => cookie
+            }
+        end
+
+        res = req.post2(uri.path, params.query, headers)
         if MHTTP.is_json(res.body)
             return res, JSON.parse(res.body)
         else
