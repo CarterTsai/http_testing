750e97c735f789a1fa75d99c5a0b7ad9553c8742
Carter Tsai <hamming1@gmail.com>
2014-10-09 17:04:01 +0800

    Add Support HTTPS and Update Get method

--- lib.rb
+++ lib.rb
@@ -1,21 +1,27 @@
 require 'net/http'
+require 'net/https'
 require 'json'
 require "addressable/uri"
+require "yaml"
 
 module MHTTP
     def MHTTP.post(uri, data, header=nil)
         req = Net::HTTP.new(uri.host, uri.port)
+
+        if uri.class.to_s === 'URI::HTTPS' 
+            req.use_ssl = (uri.scheme = 'https')
+            req.verify_mode = OpenSSL::SSL::VERIFY_NONE 
+        end
         params = Addressable::URI.new
         params.query_values = data
-
+        
         if header != nil
-            cookie = header.split('; ')[0]
-            puts cookie
+            cookie = header
             headers = {
                 'Cookie' => cookie
             }
         end
-
+        
         res = req.post2(uri.path, params.query, headers)
         if MHTTP.is_json(res.body)
             return res, JSON.parse(res.body)
@@ -23,9 +29,21 @@ module MHTTP
             return res, ""
         end
     end
-    def MHTTP.get(url, params)
-        url.query = URI.encode_www_form(params)
-        res = Net::HTTP.get_response(url)
+
+    def MHTTP.get(url, path, params, header=nil)
+        _url = url.to_s + path.to_s
+        puts _url
+        uri = URI.parse(_url)
+        http = Net::HTTP.start(uri.host, uri.port)
+
+        if header != nil
+            cookie = header.split('; ')[0]
+            headers = {
+                'Cookie' => cookie
+            }
+        end
+
+        res = http.send_request('GET', uri.request_uri, URI.encode_www_form(params) ,headers)
         if MHTTP.is_json(res.body)
             return res, JSON.parse(res.body)
         else
@@ -36,5 +54,16 @@ module MHTTP
         return JSON.parse data
     rescue
         false
+    end
+end
+
+module MCONFIG
+    @filepath = ""
+    def MCONFIG.load(filepath)
+        @filepath = filepath
+        return YAML.load_file(filepath)
+    end
+    def MCONFIG.write(yaml_data)
+        File.open(@filepath.to_s, 'w') {|f| f.write yaml_data.to_yaml}
     end
 end
